0f30bdf7bb7905fa1415e73ee981c2fd3a4778dd
Paul Rowe <prowe@sourceallies.com>
2024-06-07 21:08:45 -0500

    more recipies and support grouping

--- src/app/recipes/[filePath]/page.tsx
+++ src/app/recipes/[filePath]/page.tsx
@@ -1,46 +1,84 @@
 import { getAllRecipePaths, loadRecipe } from "@/recipes-loader";
-import styles from './page.module.css';
+import styles from "./page.module.css";
+import { Recipe } from "@/recipe-schema";
 
 type RecipePageParams = {
-    filePath: string;
-}
+  filePath: string;
+};
 
 export async function generateStaticParams(): Promise<RecipePageParams[]> {
-    const filePaths = await getAllRecipePaths();
-    return filePaths.map(filePath => ({filePath}));
+  const filePaths = await getAllRecipePaths();
+  return filePaths.map((filePath) => ({ filePath }));
 }
 
-export default async function RecipePage({params}: {params: RecipePageParams}) {
-    const recipe = await loadRecipe(params.filePath);
+function IngredientEntry({
+  ingredient,
+}: {
+  ingredient: Recipe["ingredients"][0];
+}) {
+  if (typeof ingredient === "string") {
+    return <li>{ingredient}</li>;
+  }
 
-    return (
-        <main className={styles.main}>
-            <h1>{recipe.title}</h1>
+  return (
+    <li className={styles.ingredientGroup}>
+      <h3>{ingredient.title}</h3>
+      <ul>
+        {ingredient.ingredients.map((ing) => (
+          <li key={ing}>{ing}</li>
+        ))}
+      </ul>
+    </li>
+  );
+}
 
-            <section className={styles.ingredients}>
-                <h2>Ingredients</h2>
-                <ul>
-                    {recipe.ingredients.map((ing, index) => <li key={index}>{ing}</li>)}
-                </ul>
-            </section>
+export default async function RecipePage({
+  params,
+}: {
+  params: RecipePageParams;
+}) {
+  const recipe = await loadRecipe(params.filePath);
 
-            <section className={styles.steps}>
-                <h2>Steps</h2>
-                <ol>
-                    {recipe.steps.map((step, index) => <li key={index}>{step}</li>)}
-                </ol>
-            </section>
+  return (
+    <main className={styles.main}>
+      <h1>{recipe.title}</h1>
 
-            {Boolean(recipe.notes?.length) && <section className={styles.notes}>
-                <h2>Notes</h2>
-                <ol>
-                    {recipe.notes?.map((note, index) => <li key={index}>{note}</li>)}
-                </ol>
-            </section>}
+      <section className={styles.ingredients}>
+        <h2>Ingredients</h2>
+        <ul>
+          {recipe.ingredients.map((ing, index) => (
+            <IngredientEntry key={index} ingredient={ing} />
+          ))}
+        </ul>
+      </section>
 
-            <section>
-                {recipe.source && <a href={recipe.source.toString()} target="_blank">Source</a>}
-            </section>
-        </main>
-    )
+      <section className={styles.steps}>
+        <h2>Steps</h2>
+        <ol>
+          {recipe.steps.map((step, index) => (
+            <li key={index}>{step}</li>
+          ))}
+        </ol>
+      </section>
+
+      {Boolean(recipe.notes?.length) && (
+        <section className={styles.notes}>
+          <h2>Notes</h2>
+          <ol>
+            {recipe.notes?.map((note, index) => (
+              <li key={index}>{note}</li>
+            ))}
+          </ol>
+        </section>
+      )}
+
+      <section>
+        {recipe.source && (
+          <a href={recipe.source.toString()} target="_blank">
+            Source
+          </a>
+        )}
+      </section>
+    </main>
+  );
 }
--- src/recipe-schema.ts
+++ src/recipe-schema.ts
@@ -1,13 +1,21 @@
 import { z } from "zod";
 
 export const recipeSchema = z.object({
-    title: z.string(),
-    source: z.custom<URL | null>(val => val ? new URL(val) : null).optional(),
-    ingredients: z.array(z.string()),
-    steps: z.array(z.string()),
-    notes: z.array(z.string()).optional()
+  title: z.string(),
+  source: z.custom<URL | null>((val) => (val ? new URL(val) : null)).optional(),
+  ingredients: z.array(
+    z.union([
+      z.string(),
+      z.object({
+        title: z.string(),
+        ingredients: z.array(z.string()),
+      }),
+    ])
+  ),
+  steps: z.array(z.string()),
+  notes: z.array(z.string()).optional(),
 });
 
 export type Recipe = z.infer<typeof recipeSchema> & {
-    filePath: string;
+  filePath: string;
 };
